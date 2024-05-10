fe0597ea425cb1e7a21fe0401a6500e2ab4dbd95
NgocAnh <hiromansofaoi@gmail.com>
2024-05-10 17:25:56 +0700

    new commit

--- src/app/duck/page.tsx
+++ src/app/duck/page.tsx
@@ -1,9 +1,10 @@
 "use client"
 
-export default function Home() {
+export default function Roulette() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Home
+     
+      <h1>Duck</h1>
     </main>
   );
 }
---
+++ src/app/roulette/page.tsx
@@ -0,0 +1,34 @@
+"use client"
+import weapons from "../../weapons.json";
+import McRoulette from "../../Components/Roulette/Roulette";
+import { Weapon } from "../../roulette.classes";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+export default function Roulette() {
+  const weaponsCount = 150
+  const transitionDuration = 10
+  return (
+    <main className="max-w-screen-xl">
+        <div className="flex justify-start items-center ">
+            <div className="p-4 sm:ml-64">
+                
+                <div >
+                    <McRoulette
+                    weapons={weapons as Weapon[]}
+                    weaponsCount={weaponsCount}
+                    transitionDuration={transitionDuration}
+                    />
+               
+                </div>
+            </div>
+        </div>
+      
+      
+    </main>
+  );
+}
